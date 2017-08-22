b729fc55ffadb898f201a70ed963c026031b98e3
zhhong.cai <zhhong.cai@gmail.com>
2017-08-22 23:14:05 +0800

    update project structure

--- kss-base-server/src/main/java/services/Application.java
+++ kss-base-server/src/main/java/services/Application.java
@@ -2,10 +2,16 @@ package services;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.sidecar.EnableSidecar;
 
 @SpringBootApplication
 @EnableEurekaServer
+@EnableConfigServer
+@EnableDiscoveryClient
+@EnableSidecar
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
